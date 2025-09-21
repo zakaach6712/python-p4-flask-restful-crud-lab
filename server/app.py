d461b87feb110594e5a5330c38ef168c603b2ab1
zakaachzack@gmail.com <zakaachzack@gmail.com>
2025-09-21 17:40:31 +0300

    Implement PATCH and DELETE routes for plants API

--- server/app.py
+++ server/app.py
@@ -44,8 +44,32 @@ api.add_resource(Plants, '/plants')
 class PlantByID(Resource):
 
     def get(self, id):
-        plant = Plant.query.filter_by(id=id).first().to_dict()
-        return make_response(jsonify(plant), 200)
+        plant = Plant.query.filter_by(id=id).first()
+        if not plant:
+            return make_response({"error": "Plant not found"}, 404)
+        return make_response(plant.to_dict(), 200)
+
+    def patch(self, id):
+        plant = Plant.query.filter_by(id=id).first()
+        if not plant:
+            return make_response({"error": "Plant not found"}, 404)
+
+        data = request.get_json()
+        for key, value in data.items():
+            if hasattr(plant, key):
+                setattr(plant, key, value)
+
+        db.session.commit()
+        return make_response(plant.to_dict(), 200)
+
+    def delete(self, id):
+        plant = Plant.query.filter_by(id=id).first()
+        if not plant:
+            return make_response({"error": "Plant not found"}, 404)
+
+        db.session.delete(plant)
+        db.session.commit()
+        return make_response('', 204)
 
 
 api.add_resource(PlantByID, '/plants/<int:id>')
